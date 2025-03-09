af8dac7961a90c181b43a16492b8a8d9f631d227
Abigail056 <zlyondynasty01@gmail.com>
2025-03-09 20:10:55 +0300

    Add files via upload

---
+++ Arrays/Arrays.cpp
@@ -0,0 +1,21 @@
+//c  ARRAYS
+/*
+c redefined functions
+Name:Abigail Muthoni
+Reg no: BBIT-05-0019/2024
+Git username:Abigail056
+Campus:TRC
+version
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[6]={10,20,30,40,50,60};
+    int i;
+    for(i=0; i<6; i++){
+		 cout<<marks[i] <<"\t";
+		
+    
+    cout<<endl;}
+    return 0;
+}
